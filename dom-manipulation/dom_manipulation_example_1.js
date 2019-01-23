b3aa86ef9ce364f071531eaa357eda56f0f491b0
Arnar Leifsson <drleifz@gmail.com>
2019-01-23 23:18:52 +0000

    Modifying the class demonstration a little bit

--- dom-manipulation/dom_manipulation_example_1.js
+++ dom-manipulation/dom_manipulation_example_1.js
@@ -10,21 +10,57 @@ function changeStyle(items, style, value) {
 }
 
 // 1. Select an element with the id my-item and change the background color to yellow
+var myItem = document.getElementById('my-item');
+changeStyle(myItem, 'backgroundColor', 'yellow');
 
 // 2. Select all elements with the class triangle and change the background color to blue
+var triangles = document.getElementsByClassName('triangle');
+changeStyle(triangles, 'backgroundColor', 'blue');
 
 // 3. Remove element by id my-item using old method and new method
+//myItem.remove();
+//myItem.parentNode.removeChild(myItem);
 
 // 4. Get all paragraphs and add border around them
+var paragraphs = document.getElementsByTagName('p');
+changeStyle(paragraphs, 'border', 'dashed 2px rgba(155, 155, 155, .5)');
 
 // 5. Change text content from all paragraphs with it's data-zup attributes value
+var paragraphsWithDataZup = document.querySelectorAll('p[data-zup]');
+for (var i = 0; i < paragraphsWithDataZup.length; i++) {
+    var currentElement = paragraphsWithDataZup[i];
+    var data = currentElement.dataset;
+    currentElement.textContent = data.zup;
+}
 
 // 6. Add class name fiery to all even <tr>
+var evenTableRows = document.querySelectorAll('tr:nth-child(even)');
+for (var i = 0; i < evenTableRows.length; i++) {
+    evenTableRows[i].classList.add('fiery');
+}
 
 // 7. Select password input by name and change it's type
+document.querySelector('input[name="password"]').setAttribute('type', 'text');
 
 // 8. Select all elements with class name red and change it to blue
+var redClassElements = document.getElementsByClassName('red');
+
+for (var i = 0; i < redClassElements.length; i++) {
+    redClassElements[i].classList.add('blue');
+}
+
+var blueClassElements = document.getElementsByClassName('blue');
+
+for (var i = 0; i < blueClassElements.length; i++) {
+    blueClassElements[i].classList.remove('red');
+}
 
 // 9. Add a paragraph element to div with id my-item
+var paragraph = document.createElement('p');
+paragraph.textContent = 'My new paragraph!';
+myItem.appendChild(paragraph);
 
 // 10. Replace newly created paragraph with h1
+var heading = document.createElement('h1');
+heading.textContent = 'My new heading!';
+myItem.replaceChild(heading, paragraph);
--- event-handlers/event_handlers_example_1.js
+++ event-handlers/event_handlers_example_1.js
@@ -2,6 +2,13 @@
     An example on how to work with event handlers (1)
 */
 
+function changeStyle(items, style, value) {
+    var convertedItems = HTMLCollection.prototype.isPrototypeOf(items) ? items : [items];
+    for (var i = 0; i < convertedItems.length; i++) {
+        convertedItems[i].style[style] = value;
+    }
+}
+
 // 1. Register event handlers for all input tags
 
 // 2. Register document on load (DOMContentLoaded)
@@ -10,10 +17,12 @@
 
 // 4. Register multiple event handlers with attachEvent and check the invocation order
 
-// 5. Register mouseover event handler for a specific button
+// 5. Register mouseover event handler for a specific button and change the transform origin based on the x and y coordinates within the event and add rotation class
 
-// 6. Register a parent handler on a div with class parent
+// 6. Register a parent handler on the form and also on the body
 
-// 7. Register a child handler within the div with class child and prevent bubbling vs. NOT
+// 7. Register a child handler on an input tag with the id input-4 and prevent bubbling vs. NOT
 
 // 8. Click the button, but prevent it from submitting the form
+
+// 9. Instead submit the form in code, after filling out hidden fields with 'kensentme'
