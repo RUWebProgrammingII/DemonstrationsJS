a3b21b8bb95db18e692fb066c44f41b0fb075735
Arnar Leifsson <drleifz@gmail.com>
2019-01-29 21:16:54 +0000

    Scripted HTTP and Scripting CSS demonstrations

--- scripted-http/scripted_http_example_1.js
+++ scripted-http/scripted_http_example_1.js
@@ -2,17 +2,27 @@
  * An example of how to issue XMLHttpRequests in JS
  */
 
-var rootUrl = 'https://serene-island-81305.herokuapp.com';
+var apisUrl = 'https://apis.is/concerts';
+var herokuUrl = 'https://serene-island-81305.herokuapp.com';
 
 /* Part 1 - A normal GET request */
 
     // 1. Create a basic XMLHttpRequest object
+    var basicRequest = new XMLHttpRequest();
 
-    // 2. Use the open() function as a GET request to Heroku app
+    // 2. Use the open() function as a GET request to APIS url
+    basicRequest.open('GET', apisUrl);
 
     // 3. Register the onreadystatechange event handler
+    basicRequest.onreadystatechange = function() {
+        if (basicRequest.readyState === XMLHttpRequest.DONE) {
+            var json = JSON.parse(basicRequest.responseText);
+            console.log(json);
+        }
+    };
 
     // 4. Use the send() function to issue the request
+    basicRequest.send();
 
     // 5. Examine the request within the event handler
 
@@ -21,24 +31,36 @@ var rootUrl = 'https://serene-island-81305.herokuapp.com';
 /* Part 2 - A normal POST request */
 
     // 1. Create another XMLHttpRequest
+    var postRequest = new XMLHttpRequest();
 
     // 2. Use the open() function as a POST request to Heroku app
+    postRequest.open('POST', herokuUrl + '/api/200');
 
     // 3. Set the request header to application/json
+    postRequest.setRequestHeader('Content-Type', 'application/json');
 
     // 4. Register the handler
+    postRequest.onreadystatechange = function() {
+        if (postRequest.readyState === XMLHttpRequest.DONE) {
+            console.log(JSON.parse(postRequest.responseText));
+        }
+    };
 
     // 5. Use the send() function with the body set
+    postRequest.send(JSON.stringify({ a: 1, b: 2 }));
 
     // 6. alert if successful
 
 /* Part 3 - A synchronous request */
 
     // 1. Create a basic XMLHttpRequest object
+    var synchronousRequest = new XMLHttpRequest();
 
     // 2. Use the open() function as a GET request to special method in Heroku app
+    synchronousRequest.open('GET', herokuUrl + '/api/timeout/5000', false /* Not asynchronous */);
 
     // 3. Show effects in browser
+    //synchronousRequest.send();
 
 /* Part 4 - Demonstrate encoding techniques */
 
@@ -73,8 +95,22 @@ var rootUrl = 'https://serene-island-81305.herokuapp.com';
         return formUrlEncoded.join('&');
     };
 
+    console.log(createFormUrlEncoded(form));
+    console.log(createFormData(form));
+
 /* Part 5 - Demonstrate the progress event */
 
     // 1. Issue request to special synchronous method /file
+    var progressRequest = new XMLHttpRequest();
+    progressRequest.open('GET', herokuUrl + '/api/file');
+
+    var progress = document.getElementsByClassName('progress')[0];
 
     // 2. Show the results in a progress bar
+    progressRequest.onprogress = function(e) {
+        if (e.lengthComputable) {
+            progress.style.width = (100 * (e.loaded / e.total)) + '%';
+        }
+    };
+
+    progressRequest.send();
--- scripting-css/scripting_css_example.js
+++ scripting-css/scripting_css_example.js
@@ -1,22 +1,39 @@
 /**
  * Demonstrations on how to use JS to script CSS
  */
- 
- document.getElementsByClassName('beautiful')[0].addEventListener('mousemove', function (evt) {
-     this.style.boxShadow = `${-evt.offsetX + 50}px ${-evt.offsetY + 50}px 20px 10px rgba(155, 155, 155, .5)`;
- });
 
 /* Part 1 - Basic style changes */
+    var body = document.getElementsByTagName('body')[0];
     // 1. Change color of background on body
+    body.style.backgroundColor = 'blue';
 
     // 2. Change font size of all body
+    body.style.fontSize = '24px';
 
     // 3. Toggle class (using classList and className)
+    body.classList.toggle('container');
+    body.classList.toggle('container');
+
+    var div = document.getElementsByClassName('placeholder')[0];
+    div.className += ' beautiful';
+
+    document.getElementsByClassName('beautiful')[0].addEventListener('mousemove', function (evt) {
+        this.style.boxShadow = `${-evt.offsetX + 50}px ${-evt.offsetY + 50}px 20px 10px rgba(155, 155, 155, .5)`;
+    });
 
 /* Part 2 - Using computed styles */
     var p = document.getElementById('text');
     // 1. Get computed style for p with id text
+    console.log(getComputedStyle(p, null));
 
     // 2. Create shrink function
+    p.nextElementSibling.addEventListener('click', function (e) {
+        var fontSize = getComputedStyle(p, null).fontSize;
+        p.style.fontSize = (parseInt(fontSize.substring(0, fontSize.length - 2)) - 1) + 'px';
+    });
 
     // 3. Create enlarge function
+    p.previousElementSibling.addEventListener('click', function (e) {
+        var fontSize = getComputedStyle(p, null).fontSize;
+        p.style.fontSize = (parseInt(fontSize.substring(0, fontSize.length - 2)) + 1) + 'px';
+    });
