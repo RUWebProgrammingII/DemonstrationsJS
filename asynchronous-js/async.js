9abc61e54894aa5bf8700d79242f53861e4580cc
Arnar Leifsson <arnarl@ru.is>
2019-02-12 14:03:32 +0000

    Adding asynchronous implementation

--- asynchronous-js/async.js
+++ asynchronous-js/async.js
@@ -31,10 +31,37 @@ function getNextSequence() {
 }
 
 // 1. Use getNextSequence to count to four using chained promises
+// getNextSequence().then(function (number) {
+// 	console.log(number);
+// 	return getNextSequence();
+// }).then(function (number) {
+// 	console.log(number);
+// 	return getNextSequence();
+// }).then(function (number) {
+// 	console.log(number);
+// 	return getNextSequence();
+// }).then(function (number) {
+// 	console.log(number);
+// });
 
 // 2. Convert the chained promises to async / await instead
+// (async function() {
+// 	console.log(new Date());
+// 	var one = await getNextSequence();
+// 	var two = await getNextSequence();
+// 	var three = await getNextSequence();
+// 	var four = await getNextSequence();
+// 	console.log(new Date());
+// 	console.log(one, two, three, four);
+// })();
 
 // 3. Convert the async / await to Promise.all
+// (async function() {
+// 	console.log(new Date());
+// 	var results = await Promise.all([ getNextSequence(), getNextSequence(), getNextSequence(), getNextSequence() ]);
+// 	console.log(new Date());
+// 	console.log(results);
+// })();
 
 // 4. Use Date() to measure the time for 2. and 3.
 
@@ -51,13 +78,37 @@ var companyName = 'Bláa lónið hf.';
 // Conditionals
 
 // 1. Get the company Bláa lónið hf. and check if it exists using promises
+getIcelandicCompanyByName(companyName).then(function (data) {
+	if (data.hasOwnProperty('results') && data['results'].length > 0) {
+		console.log('It exists!');
+	} else {
+		throw new Error('It doesn\'t exist!');
+	}
+});
 
 // 2. Instead of using promises use async / await
+(async function () {
+	var data = await getIcelandicCompanyByName(companyName);
+	if (data.hasOwnProperty('results') && data['results'].length > 0) {
+		console.log('It exists!');
+	} else {
+		console.log('it doesn"t exist')
+	}
+})();
 
 // Error handling
 var dukeNukem = 'Duke Nukem 3D hf.';
 
 // 1. Get the company Duke Nukem 3D hf. and check if it exists using promises,
 //    if the company does not exist an Error should be thrown
+getIcelandicCompanyByName(dukeNukem).then(function (data) {
+	if (data.hasOwnProperty('results') && data['results'].length > 0) {
+		console.log('It exists!');
+	} else {
+		console.log('It doesn\'t exist!');
+	}
+}, function (err) {
+	console.log(err);
+});
 
 // 2. Do the same thing as above, but now using async / await
--- js-arrays/arrays.js
+++ js-arrays/arrays.js
@@ -5,10 +5,16 @@
 /* Basic methods */
 
     // 1. Join an array with -
+    var arr = [1, 2, 3, 4];
+    console.log(arr.join('-'));
 
     // 2. Reverse an array
+    arr.reverse();
+    console.log(arr);
 
     // 3. Sort an array
+    arr.sort();
+    console.log(arr);
 
     // 4. Sort an array with a reverse sort function
 
@@ -19,20 +25,36 @@
 /* More advanced methods */
 
     // 1. Map a square function
+    var arrTwo = [5, 6, 7, 8, 9];
+    console.log(arrTwo.map((elem) => elem * 2));
 
     // 2. Map a function which takes an array of lowercase brands and makes all uppercase
+    var uppercaseBrands = ['puma', 'nike', 'adidas'].map(function (elem) { return elem.charAt(0).toUpperCase() + elem.slice(1); });
+    console.log(uppercaseBrands);
 
     // 3. Filter a function for valid SSN with the correct length
+    var validSsns = [ '1205132060', '1011084870', 'INVALIDSSN' ].filter(function (elem) { return /[0-9]{10}/g.test(elem); });
+    console.log(validSsns);
 
     // 4. Filter all odd numbers which are larger than 5
+    var oddNumbersLargerThanFive = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].filter(function (elem) {
+        return elem >= 5 && elem % 2 !== 0;
+    });
+    console.log(oddNumbersLargerThanFive);
 
     // 5. Evaluate if every element are a typeof function (should pass)
+    console.log([() => { }, () => {}, { x: 1 }].every(function(elem) {
+        return typeof(elem) === 'function';
+    }));
 
     // 6. Evaluate if every element is larger than 10
 
     // 7. Evaluate if some elements are squared equal to 4
 
     // 8. Reduce all elements and calculate the average
+    var arr = [1, 2, 3, 4, 5, 6];
+    var average = arr.reduce(function (elem, total) { return elem + total }, 0) / arr.length;
+    console.log(average);
 
     // 9. Reduce a reversed string
 
